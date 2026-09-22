d03bc4bd1f2c1049b1a6539c43bdfbdc2fef6746
Дмитро Касьян <dimakasan028@gmail.com>
2026-09-22 09:29:37 +0300

    Add files via upload

---
+++ scripts/values.js
@@ -0,0 +1,50 @@
+/**
+ * Дія - Конфігураційні змінні та стан за замовчуванням
+ */
+var entryPin = '1234';
+var allowBiometricSkip = true;
+
+var isRightsEnabled = true;
+var isStudyEnabled = true;
+var isZagranEnabled = true;
+var isDiplomaEnabled = true;
+
+// Початкові дані особи
+var defaultUserData = {
+    textName: "Дмитро",
+    name: "КОЦЮБА ДМИТРО ОЛЕКСАНДРОВИЧ",
+    nameEn: "KOTSIUBA DMYTRO",
+    birthDate: "12.07.1999",
+    rnokpp: "3635201948",
+    nomerPasport: "008759371",
+    sex: "Ч",
+    sexEn: "M",
+    dateGive: "12.02.2020",
+    dateOut: "12.02.2030",
+    organ: "1340",
+    uznr: "19990712-07344",
+    placeBirth: "КИЇВ",
+    legalAdress: "м. Київ, вул. Хрещатик, буд. 22",
+    registeredOn: "14.05.2015",
+    zagran_number: "FT306260",
+    dateGiveZ: "21.12.2022",
+    dateOutZ: "21.12.2032",
+    nomerStudy: "КВ №1948201",
+    vidanoStudy: "01.09.2021",
+    diusnuyDoStudy: "30.06.2025",
+    formaStudy: "Денна",
+    university: "Державний університет «Київський політехнічний інститут»",
+    fakultat: "Факультет інформатики та обчислювальної техніки",
+    pravaNnumber: "KBE968639",
+    rightsCategories: "A, B, C1",
+    dateGivePrava: "12.02.2020",
+    srokPrav: "12.02.2050",
+    pravaOrgan: "ТСЦ 8631",
+    zbroyaType: "Пістолет ПМ (9-мм пістолет Макарова)",
+    zbroyaNumber: "0000009965",
+    stepen_dip: "Диплом бакалавра",
+    univer_dip: "Національний технічний університет України",
+    dayout_dip: "30.06.2023",
+    special_dip: "122 Комп'ютерні науки",
+    number_dip: "В23 049182"
+};
